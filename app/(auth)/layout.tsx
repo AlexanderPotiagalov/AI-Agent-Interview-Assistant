c7f12ab8cf18428a60853b529ce933202e748c4d
AlexanderPotiagalov <apa168@sfu.ca>
2025-06-11 19:45:23 -0700

    Implemented getCurrentUser and isAuthenticated to auth.action.ts & utilized in (root) layout.tsx and (auth) layout.tsx for correct redirection.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
 import { ReactNode } from "react";
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next/navigation";
 
 // destructor children from props and making children type ReactNode
-const AuthLayout = ({ children }: { children: ReactNode }) => {
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (isUserAuthenticated) redirect("/");
   return <div className="auth-layout">{children}</div>;
 };
 
